cf1b279067ee9bdea38f0be0847fb5a5d3e2f069
Adminjai <microaxle@outlook.com>
2026-04-26 20:52:33 +0530

    updated

--- nha-templates/objects/utility_scripts/showerrors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Match AMQ errors with the time it happened
-#
-# Reference utility script
-#
-qmgr=$(dspmq |awk -F'[()]' '{print $2}')
-egrep '^[0-9]{2}/[0-9]{2}/[0-9]{2}|^AMQ' /var/mqm/qmgrs/${qmgr}/errors/AMQERR01.LOG |grep -B 1 ^AMQ |awk '/AMQ/ { printf " %s", $0; next} {if (NR>1) printf "\n"; printf "%s", $0}'
-echo
